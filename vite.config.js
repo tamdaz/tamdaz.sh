3606c8fe21cb24f2e6ee01bd0c2d43c1ffc27c33
tamdaz <tamda.zohir.pro@gmail.com>
2024-08-17 16:38:59 +0200

    Eviter le rechargement à chaud.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: false
-  }
+	plugins: [react()],
+	server: {
+		hmr: false
+	}
 })
