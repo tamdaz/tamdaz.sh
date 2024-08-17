85d29071ee666679a6f54e580b6826e8a29a2496
tamdaz <tamda.zohir.pro@gmail.com>
2024-08-17 19:03:27 +0200

    Refactorisation du composant Root.

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,35 +4,42 @@ import Window from './Window';
 import Loading from './apps/Loading';
 import Terminal from './apps/Terminal';
 
-import useTyper from './hooks/useTyper';
 import { loadColors } from "./commands/color";
 
 export const WindowContext = React.createContext(null);
 
 export default function Root() {
 	const [windows, setWindows] = React.useState([]);
-	const value = useTyper("// Cette version est en cours de développement, il n'est pas encore finie.");
 
 	React.useEffect(() => {
 		/** @param { KeyboardEvent } e */
 		const event = window.addEventListener("keydown", (e) => {
-			if (e.ctrlKey && e.key === "Enter") {
-				setWindows((oldWindows) => [...oldWindows, {
-					id: "window-terminal",
-					title: "Terminal",
-					initialX: 75,
-					initialY: 75,
-					initialWidth: 16 * 45,
-					initialHeight: 16 * 20,
-					view: <Terminal />
-				}])
+			if (e.ctrlKey && e.key === "Enter" && !/\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(navigator.userAgent)) {
+				setWindows((oldWindows) => {
+					if (oldWindows.some(window => window.id === "window-terminal"))
+						return oldWindows;
+
+					return [...oldWindows, {
+						id: "window-terminal",
+						title: "Terminal",
+						initialX: window.innerWidth / 2 - (16 * 45 / 2),
+						initialY: window.innerHeight / 2 - (16 * 20 / 2),
+						initialWidth: 16 * 45,
+						initialHeight: 16 * 20,
+						view: <Terminal />
+					}];
+				});
+
+				setTimeout(() => {
+					setWindows((oldWindows) => oldWindows.filter(window => window.id !== "window-loading"));
+				}, 2000);
 			}
 		});
 
 		return () => {
 			window.removeEventListener("keydown", event);
 		}
-	}, []);
+	}, [windows]);
 
 	React.useEffect(() => {
 		loadColors();
@@ -46,7 +53,8 @@ export default function Root() {
 				initialY: window.innerHeight / 2 - (16 * 5 / 2),
 				initialWidth: 16 * 30,
 				initialHeight: 16 * 5,
-				view: <Loading />
+				view: <Loading />,
+				closeable: false
 			}])
 		}, 1500);
 	}, []);
@@ -65,14 +73,11 @@ export default function Root() {
 							initialX={window.initialX}
 							initialY={window.initialY}
 							view={window.view}
+							closeable={window.closeable}
 						/>
 					})
 				}
 			</WindowContext.Provider>
-		</div>
-		<div style={{ zIndex: -9, width: "100%", height: "100vh", padding: "12pt", display: "flex", flexDirection: "column", position: "absolute" }}>
-			<span>{ value }</span>
-			<span>Nombre de fenêtres ouvertes: { windows.length }</span>
 		</div>
     </>
 }
