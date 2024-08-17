5b8d818950a7daa5a20a0ebbf0a1ffffc11ec568
tamdaz <tamda.zohir.pro@gmail.com>
2024-08-17 19:04:37 +0200

    Informer aux utilisateurs que ce site est uniquement utilisée en version ordinateur. Refactorisation du composant Loading.

--- src/apps/Loading.jsx
+++ src/apps/Loading.jsx
@@ -11,34 +11,46 @@ export default function Loading() {
         const speed = 30;
         const progressBarWidth = 50;
 
-        setTimeout(() => {
-            new Audio(boot).play();
-
-            for (let i = 0; i <= progressBarWidth; i++) {
-                setTimeout(() => {
-                    loadingRef.current.innerHTML = "[";
-                    loadingRef.current.innerHTML += "#".repeat(i);
-                    loadingRef.current.innerHTML += "-".repeat(progressBarWidth - i);
-                    loadingRef.current.innerHTML += "]";
-                }, speed * (i + 1));
-            }
-        }, 1500);
-
-        setTimeout(() => {
-            new Audio(background_music).play();
-
-            loadingRef.current.innerHTML = "[&nbsp;";
-            loadingRef.current.innerHTML += "Bienvenue sur mon portfolio version terminal."
-            loadingRef.current.innerHTML += "&nbsp;".repeat(progressBarWidth - 46);
-            loadingRef.current.innerHTML += "]";
-        }, 1500 + speed * progressBarWidth + 100);
+        // Only as a last resort, fall back to user agent sniffing
+        const UA = navigator.userAgent;
         
-        setTimeout(() => {
-            loadingRef.current.innerHTML = "[&nbsp;";
-            loadingRef.current.innerHTML += "Ctrl+Entrée pour ouvrir un terminal."
-            loadingRef.current.innerHTML += "&nbsp;".repeat(progressBarWidth - 37);
-            loadingRef.current.innerHTML += "]";
-        }, 1500 + speed * progressBarWidth + 2000);
+        if (/\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA)) {
+            setTimeout(() => {
+                loadingRef.current.innerHTML = "[&nbsp;";
+                loadingRef.current.innerHTML += "Ordinateur nécessaire pour accéder à tamdaz.sh"
+                loadingRef.current.innerHTML += "&nbsp;".repeat(progressBarWidth - 47);
+                loadingRef.current.innerHTML += "]";
+            }, 1500);
+        } else {
+            setTimeout(() => {
+                new Audio(boot).play();
+
+                for (let i = 0; i <= progressBarWidth; i++) {
+                    setTimeout(() => {
+                        loadingRef.current.innerHTML = "[";
+                        loadingRef.current.innerHTML += "#".repeat(i);
+                        loadingRef.current.innerHTML += "-".repeat(progressBarWidth - i);
+                        loadingRef.current.innerHTML += "]";
+                    }, speed * (i + 1));
+                }
+            }, 1500);
+
+            setTimeout(() => {
+                new Audio(background_music).play();
+
+                loadingRef.current.innerHTML = "[&nbsp;";
+                loadingRef.current.innerHTML += "Bienvenue sur \"tamdaz.sh\"."
+                loadingRef.current.innerHTML += "&nbsp;".repeat(progressBarWidth - 27);
+                loadingRef.current.innerHTML += "]";
+            }, 1500 + speed * progressBarWidth + 100);
+
+            setTimeout(() => {
+                loadingRef.current.innerHTML = "[&nbsp;";
+                loadingRef.current.innerHTML += "Faites Ctrl+Entrée pour ouvrir un terminal."
+                loadingRef.current.innerHTML += "&nbsp;".repeat(progressBarWidth - 44);
+                loadingRef.current.innerHTML += "]";
+            }, 1500 + speed * progressBarWidth + 2000);
+        }
     }, []);
 
     return <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%" }}>
