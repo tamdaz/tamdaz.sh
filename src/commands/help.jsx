b2796dd23bb9dbdfa65b4261098eab72893991e9
tamdaz <tamda.zohir.pro@gmail.com>
2024-08-17 19:06:43 +0200

    Mise à jour de la commande "help".

--- src/commands/help.jsx
+++ src/commands/help.jsx
@@ -1,13 +1,18 @@
 export const displayHelp = () => {
     const helpOutput = <>
-        <span>&gt;&gt;&gt; AIDE COMMANDES.</span>
+        <span>&gt;&gt;&gt; AIDE COMMANDES</span>
+        <span>&gt;&gt; Commandes principales :</span>
         <span>aboutme      --&gt; Affiche les informations à propos de moi.</span>
-        <span>clear        --&gt; Nettoie le terminal.</span>
-        <span>color        --&gt; Change de couleur.</span>
         <span>credits      --&gt; Affiche les crédits / mentions légales.</span>
+        <span>portfolio    --&gt; Aller sur mon site portfolio.</span>
+        <span>&gt;&gt; Commandes essentielles :</span>
+        <span>clear        --&gt; Nettoie le terminal.</span>
+        <span>historyc     --&gt; Nettoie l'historique des commandes.</span>
         <span>exit         --&gt; Ferme le terminal.</span>
         <span>help         --&gt; Affiche la commande d'aide.</span>
-        <span>portfolio    --&gt; Aller sur mon site portfolio.</span>
+        <span>&gt;&gt; Autres commandes :</span>
+        <span>color        --&gt; Change de couleur.</span>
+        <span>brightness   --&gt; Modifie la luminosité de l'écran.</span>
     </>
 
     return helpOutput;
